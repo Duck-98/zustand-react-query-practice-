c59e3cd47f2391f4c813ee6ddc186e398644e890
Deokgyung <devduck@mailplug.co.kr>
2023-10-13 17:10:42 +0900

    [spam] feat: 날짜 정렬 함수 분리

--- spam/spam-client/src/utils/date.ts
+++ spam/spam-client/src/utils/date.ts
@@ -1,6 +1,8 @@
+import type { Block, Restore } from '@/types';
+
 export function formatDate(dateString: string) {
   const date = new Date(dateString);
-  const year = date.getFullYear();
+  const year = date.getFullYear() % 100;
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
   const hours = String(date.getHours()).padStart(2, '0');
@@ -8,3 +10,12 @@ export function formatDate(dateString: string) {
 
   return `${year}-${month}-${day} ${hours}:${minutes}`;
 }
+
+export const sortByDate = (data, ascending: boolean) => {
+  return data.sort((a, b) => {
+    const dateA = new Date(a.restore_date || a.block_date);
+    const dateB = new Date(b.restore_date || b.block_date);
+
+    return ascending ? dateA.getTime() - dateB.getTime() : dateB.getTime() - dateA.getTime();
+  });
+};
