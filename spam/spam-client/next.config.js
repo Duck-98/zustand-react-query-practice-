97cb7bcd0cb521b78282aeffe38f5e88ea262873
Deokgyung <devduck@mailplug.co.kr>
2023-10-23 15:42:03 +0900

    [spam] feat: next proxy 설정

--- spam/spam-client/next.config.js
+++ spam/spam-client/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/auth/:path*',
+        destination: 'https://auth.mailplug.com/:path*',
+      },
+      {
+        source: '/login/:path*',
+        destination: 'https://testm01.mail-server.kr:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
