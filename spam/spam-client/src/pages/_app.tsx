76ac2de3e9a241711b764500893ec0dc290f75cb
Deokgyung <devduck@mailplug.co.kr>
2023-10-26 17:05:12 +0900

    [spam] feat: react-query queryClient 세팅

--- spam/spam-client/src/pages/_app.tsx
+++ spam/spam-client/src/pages/_app.tsx
@@ -1,14 +1,24 @@
-import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import dynamic from 'next/dynamic';
+
+const DynamicLayoutWithNoSSR = dynamic(() => import('@/components/Layout'), {
+  ssr: false,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <ToastContainer />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <DynamicLayoutWithNoSSR>
+        <Component {...pageProps} />
+        <ToastContainer />
+      </DynamicLayoutWithNoSSR>
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   );
 }
